943e7f5901bf8e661b561234cead6969977fdca3
mukesh4u <mukesh421985@gmail.com>
2014-05-25 22:56:31 +0530

    Updated License of the code

--- ActionBarTest/src/com/robotium/actionbartest/MainActivityTest.java
+++ ActionBarTest/src/com/robotium/actionbartest/MainActivityTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Mukesh Y authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.robotium.actionbartest;
 
 import android.app.Activity;
@@ -7,6 +22,9 @@ import com.example.actionbar.MainActivity;
 import com.example.actionbar.Map;
 import com.robotium.solo.Solo;
 
+/**
+ * @author Mukesh Y
+ */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	private Solo solo;
 	private Activity activity;
